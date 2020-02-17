e7f45a646250aa50dc50d81f2de30db67029d61e
Marco Schmid <marco.schmid@eda.admin.ch>
2020-02-17 17:35:33 +0100

    added tooltip

---
+++ Assets/OnMouse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnMouse : MonoBehaviour
+{
+    private void OnMouseUp()
+    {
+        Debug.Log("You cliecked " + name);
+    }
+}
---
+++ Assets/WallMouse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class WallMouse : MonoBehaviour
+{
+    public GameObject tooltip; //<--Assign in inspector & deactivate.
+    public TextMeshProUGUI text;
+
+    private void OnMouseUp()
+    {
+        var mask = LayerMask.GetMask("Cubes");
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, mask))
+        {
+            var o = hit.collider.gameObject;
+            tooltip.transform.position = hit.point + Camera.main.transform.forward * -1;
+
+            text.text = o.name;
+            
+            tooltip.SetActive(true);
+        }
+    }
+}
