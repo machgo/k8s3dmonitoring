daef559d74f563eea037463a54553819476ba110
Marco Schmid <marco@marcoschmid.me>
2020-02-17 23:10:50 +0100

    added stupid buttion

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public GameObject stomper;
+    private void OnMouseUp()
+    {
+        Vector3 explosionPos = stomper.transform.position;
+        float radius = 10;
+        float power = 2000;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null)
+                rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+        }
+    }
+}
