9011eca44cc6149513768106a92e9800cd2cfdb4
Marco Schmid <marco.schmid@eda.admin.ch>
2020-02-27 16:11:40 +0100

    added timetable

---
+++ Assets/Timetable.cs
@@ -0,0 +1,68 @@
+﻿using Assets;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using TMPro;
+using UnityEngine;
+
+public class Timetable : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("UpdateTimetable", 2, 60);
+    }
+    void UpdateTimetable()
+    {
+        var info1 = GetTimetableFromApi("Europaplatz Süd");
+        var info2 = GetTimetableFromApi("Europaplatz Nord");
+        var info3 = GetTimetableFromApi("Europaplatz Bahnhof");
+
+        var stationboard = info1.stationboard;
+        stationboard.AddRange(info2.stationboard);
+        stationboard.AddRange(info3.stationboard);
+        stationboard.Sort();
+
+        leftString = "";
+        rightString = "";
+
+        // category number - to - time
+        foreach (var i in stationboard)
+        {
+            DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(i.stop.departureTimestamp);
+
+            leftString += i.category + i.number + "\t" + i.to + "\n";
+            rightString += i.stop.platform + "\t" + dateTimeOffset.LocalDateTime.ToString("HH:mm") + "\n";
+        }
+
+        leftColumnText.GetComponent<TextMeshPro>().SetText(leftString);
+        rightColumnText.GetComponent<TextMeshPro>().SetText(rightString);
+    }
+
+    private RootObject GetTimetableFromApi(string station)
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("http://transport.opendata.ch/v1/stationboard?station={0}&limit=3", station));
+        request.Proxy = new WebProxy("http://intranet.proxy.eda.admin.ch:7070");
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        StreamReader reader = new StreamReader(response.GetResponseStream());
+        string jsonResponse = reader.ReadToEnd();
+        var info = JsonUtility.FromJson<RootObject>(jsonResponse);
+        return info;
+    }
+
+
+
+    private string leftString = "";
+    private string rightString = "";
+    public GameObject leftColumnText;
+    public GameObject rightColumnText;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/TransportOpendata.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets
+{
+    [System.Serializable]
+    public class Coordinate
+    {
+        public string type;
+        public double x;
+        public double y;
+    }
+
+    [System.Serializable]
+    public class Station
+    {
+        public string id;
+        public string name;
+        public object score;
+        public Coordinate coordinate;
+        public object distance;
+    }
+
+    [System.Serializable]
+    public class Coordinate2
+    {
+        public string type;
+        public double x;
+        public double y;
+    }
+
+    [System.Serializable]
+    public class Station2
+    {
+        public string id;
+        public string name;
+        public object score;
+        public Coordinate2 coordinate;
+        public object distance;
+    }
+
+    [System.Serializable]
+    public class Prognosis
+    {
+        public object platform;
+        public object arrival;
+        public object departure;
+        public object capacity1st;
+        public object capacity2nd;
+    }
+
+    [System.Serializable]
+    public class Coordinate3
+    {
+        public string type;
+        public object x;
+        public object y;
+    }
+
+    [System.Serializable]
+    public class Location
+    {
+        public string id;
+        public object name;
+        public object score;
+        public Coordinate3 coordinate;
+        public object distance;
+    }
+
+    [System.Serializable]
+    public class Stop
+    {
+        public Station2 station;
+        public object arrival;
+        public object arrivalTimestamp;
+        public DateTime departure;
+        public int departureTimestamp;
+        public object delay;
+        public string platform;
+        public Prognosis prognosis;
+        public object realtimeAvailability;
+        public Location location;
+    }
+
+    [System.Serializable]
+    public class Coordinate4
+    {
+        public string type;
+        public double? x;
+        public double? y;
+    }
+
+    [System.Serializable]
+    public class Station3
+    {
+        public string id;
+        public string name;
+        public object score;
+        public Coordinate4 coordinate;
+        public object distance;
+    }
+
+    [System.Serializable]
+    public class Prognosis2
+    {
+        public object platform;
+        public object arrival;
+        public object departure;
+        public object capacity1st;
+        public object capacity2nd;
+    }
+
+    [System.Serializable]
+    public class Coordinate5
+    {
+        public string type;
+        public double? x;
+        public double? y;
+    }
+
+    [System.Serializable]
+    public class Location2
+    {
+        public string id;
+        public string name;
+        public object score;
+        public Coordinate5 coordinate;
+        public object distance;
+    }
+
+    [System.Serializable]
+    public class PassList
+    {
+        public Station3 station;
+        public DateTime? arrival;
+        public int? arrivalTimestamp;
+        public DateTime? departure;
+        public int? departureTimestamp;
+        public object delay;
+        public string platform;
+        public Prognosis2 prognosis;
+        public object realtimeAvailability;
+        public Location2 location;
+    }
+
+    [System.Serializable]
+    public class Stationboard : IComparable
+    {
+        public Stop stop;
+        public string name;
+        public string category;
+        public object subcategory;
+        public object categoryCode;
+        public string number;
+        public string @operator;
+        public string to;
+        public List<PassList> passList;
+        public object capacity1st;
+        public object capacity2nd;
+
+        public int CompareTo(object obj)
+        {
+            if (obj is Stationboard)
+            {
+                return this.stop.departureTimestamp.CompareTo((obj as Stationboard).stop.departureTimestamp);
+            }
+            throw new ArgumentException("Object is not a Stationboard");
+        }
+    }
+
+    [System.Serializable]
+    public class RootObject
+    {
+        public Station station;
+        public List<Stationboard> stationboard;
+    }
+}
